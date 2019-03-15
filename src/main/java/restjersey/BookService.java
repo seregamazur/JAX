0f4b3ec45154481529fab6ce01db2f001c37a866
seregamazur <serhiy.mazur0@gmail.com>
2019-03-15 16:07:50 +0200

    Working service

--- src/main/java/restjersey/BookService.java
+++ src/main/java/restjersey/BookService.java
@@ -11,20 +11,26 @@ import javax.ws.rs.core.Response;
 @Path("/books")
 public class BookService {
     MockedBooks mockedBooks = MockedBooks.getInstance();
+
     @GET
     @Produces("application/json")
     public Response getBooks() throws JSONException {
-        return Response.status(200).entity(mockedBooks.toJsonArray()).build();
+        mockedBooks.addBook(new Book("Hi", "Serhiy Mazur", 13.45f));
+        mockedBooks.addBook(new Book("Hi", "Serhiy Mazur", 13.45f));
+        mockedBooks.addBook(new Book("Hi", "Serhiy Mazur", 13.45f));
+        String result = "" + mockedBooks.toJsonArray();
+        return Response.status(200).entity(result).build();
     }
 
     @Path("{f}")
     @GET
     @Produces("application/json")
     public Response getBookById(@PathParam("f") int f) throws JSONException {
-        mockedBooks.addBook(new Book("Hi","Serhiy Mazur",13.45f));
-        mockedBooks.addBook(new Book("Hi","Serhiy Mazur",13.45f));
-        mockedBooks.addBook(new Book("Hi","Serhiy Mazur",13.45f));
-        return Response.status(200).entity(mockedBooks.getBook(f).toJson()).build();
+        mockedBooks.addBook(new Book("Hi", "Serhiy Mazur", 13.45f));
+        mockedBooks.addBook(new Book("Hi", "Serhiy Mazur", 13.45f));
+        mockedBooks.addBook(new Book("Hi", "Serhiy Mazur", 13.45f));
+        String result = "" + mockedBooks.getBook(f).toJson();
+        return Response.status(200).entity(result).build();
     }
 
 
