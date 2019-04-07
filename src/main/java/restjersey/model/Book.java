71c0c3cc5e8e644e5dc31112e059f853ecdf85c8
seregamazur <serhiy.mazur0@gmail.com>
2019-04-07 13:44:20 +0300

    Fixed deleted hibernate

--- src/main/java/restjersey/model/Book.java
+++ src/main/java/restjersey/model/Book.java
@@ -2,19 +2,11 @@ package main.java.restjersey.model;
 
 import org.json.JSONObject;
 
-import javax.persistence.*;
 
-@Entity
-@Table(name = "Book")
 public class Book {
-    @Id @GeneratedValue
-    @Column(name = "id")
     private int ID;
-    @Column(name = "name")
     private String name;
-    @Column(name = "author")
     private String author;
-    @Column(name = "price")
     private int price;
 
     public Book() {
